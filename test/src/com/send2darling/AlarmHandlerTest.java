8b32649ef0ccd80d71f5b8efdc7583cf66d56047
Hu Kai <iamkaihu@gmail.com>
2012-02-12 22:45:52 +1100

    make the tests running.

--- test/src/com/send2darling/AlarmHandlerTest.java
+++ test/src/com/send2darling/AlarmHandlerTest.java
@@ -5,13 +5,13 @@ import org.joda.time.DateTime;
 
 public class AlarmHandlerTest extends AndroidTestCase {
 
-    public void should_return_this_coming_noon_when_given_time_is_morning() {
+    public void test_should_return_this_coming_noon_when_given_time_is_morning() {
         DateTime dateTime = new DateTime(2010, 12, 12, 6, 0, 0);
         assertEquals(new AlarmHandler().morning(dateTime), new DateTime(2010, 12, 12, 8, 0, 0));
     }
 
 
-    public void should_return_the_next_morning_when_given_time_passed_morning() {
+    public void test_should_return_the_next_morning_when_given_time_passed_morning() {
         DateTime dateTime = new DateTime(2010, 12, 12, 10, 0, 0);
         assertEquals(new AlarmHandler().morning(dateTime), new DateTime(2010, 12, 13, 8, 0, 0));
     }
